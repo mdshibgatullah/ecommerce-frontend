447c57ac71235f5fcca1366322e4d8a9ab232cae
mdshibgatullah <mdshibgatullah94@gmail.com>
2026-03-11 13:21:50 +0600

    update

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -60,56 +60,56 @@ const {
             <Container className='d-flex justify-content-center py-5'>
                 <form action="" onSubmit={handleSubmit(onSubmit)}>
                 <div className="card shadow border-0 login">
+                    
                     <div className="card-body p-4">
-                        <div className="card-body p-4">
-                            <h3 className='mb-3 border-bottom pb-2'>User Login</h3>
+                        <h3 className='mb-3 border-bottom pb-2'>User Login</h3>
+                        
+                            <div className='mb-2'>
+                                <label htmlFor="" className='form-label'>Email</label>
+                                <input
+                                {
+                                    ...register('email',{
+                                        required: "The email field is required",
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "Invalid email address"
+                                        } 
+                                    })
+                                }
+                                type="text" className={`form-control ${errors.email && 'is-invalid'}`} 
+                                placeholder='Email'/>
+                                {
+                                    errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>
+                                }
+                            </div>
+
+                            <div className='mb-2'>
+                                <label htmlFor="" className='form-label'>Password</label>
+                                <input
+                                {
+                                    ...register('password',{
+                                        required: 'The password field is required'
+                                    })
+                                }
+                                type="password" className={`form-control ${errors.password && 'is-invalid'}`}  
+                                placeholder='Password'/>
+                                {
+                                    errors.password && <p className='invalid-feedback'>{errors.password?.message}</p>
+                                }
+                            </div>
+
+                            <div className="text-end my-3">
+                                <Link to="/account/forgot-password">Forgot Password?</Link>
+                            </div>
                             
-                                <div className='mb-2'>
-                                    <label htmlFor="" className='form-label'>Email</label>
-                                    <input
-                                    {
-                                        ...register('email',{
-                                            required: "The email field is required",
-                                            pattern: {
-                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                                message: "Invalid email address"
-                                            } 
-                                        })
-                                    }
-                                    type="text" className={`form-control ${errors.email && 'is-invalid'}`} 
-                                    placeholder='Email'/>
-                                    {
-                                        errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>
-                                    }
-                                </div>
+                            <button className='btn btn-secondary w-100 '>Login</button>
 
-                                <div className='mb-2'>
-                                    <label htmlFor="" className='form-label'>Password</label>
-                                    <input
-                                    {
-                                        ...register('password',{
-                                            required: 'The password field is required'
-                                        })
-                                    }
-                                    type="password" className={`form-control ${errors.password && 'is-invalid'}`}  
-                                    placeholder='Password'/>
-                                    {
-                                        errors.password && <p className='invalid-feedback'>{errors.password?.message}</p>
-                                    }
-                                </div>
-
-                                <div className="text-end my-3">
-                                    <Link to="/admin/forgot-password">Forgot Password?</Link>
-                                </div>
-                                
-                                <button className='btn btn-secondary w-100 '>Login</button>
-
-                                <div className="d-flex justify-content-center mt-3 gap-2">
-                                    Don't have an account? <Link to="/account/register">Register</Link>
-                                </div>
-                            
-                        </div>
+                            <div className="d-flex justify-content-center mt-3 gap-2">
+                                Don't have an account? <Link to="/account/register">Register</Link>
+                            </div>
+                        
                     </div>
+                    
                 </div>
                 </form>
             </Container>
